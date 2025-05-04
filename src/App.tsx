f334ef3d9bba51e26e2b9d84193c933e6c25fc11
Nadimul Rahib <nadimulrahib38@gmail.com>
2025-05-03 21:24:38 -0700

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Category from "./components/Category";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Button>Click me</Button>
+      <Category></Category>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Category.tsx
@@ -0,0 +1,18 @@
+
+const Category = () => {
+  return (
+    <div>
+       <div className="text-center justify-center items-center">
+       <p className="text-xs">CATEGORY</p>
+       <h1 className="text-4xl font-semibold">We Offer Best Services</h1>
+       </div>
+       <div>
+        <div>
+            
+        </div>
+       </div>
+    </div>
+  )
+}
+
+export default Category
